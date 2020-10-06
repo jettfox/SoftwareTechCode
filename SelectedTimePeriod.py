cc062e6d25489842be12d4b24148a74c4ad1d91c
IAmCableGuy <62466388+IAmCableGuy@users.noreply.github.com>
2020-10-06 21:00:45 +1000

    Add files via upload

---
+++ SelectedTimePeriod.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from file_loader import fileloader
+fulldata = fileloader()
+def select_Time_Period(SelDate, EndDate):
+    for line in range(len(fulldata['ACCIDENT_DATE'])):
+        fulldata['Year'][line] = fulldata['ACCIDENT_DATE'][line].split('/')[2]
+        fulldata['Month'][line] = fulldata['ACCIDENT_DATE'][line].split('/')[1]
+        fulldata['Day'][line] = fulldata['ACCIDENT_DATE'][line].split('/')[0]
+        if(line % 749 == 0):
+            print(f'{line//749}%')
+    
+    StartYear = SelDate.split('/')[0]
+    StartMonth = SelDate.split('/')[1]
+    StartDay = SelDate.split('/')[2]
+    EndYear = EndDate.split('/')[0]
+    EndMonth = EndDate.split('/')[1]
+    EndDay = EndDate.split('/')[2]
+    
+    toconcat = []
+    if (StartYear != EndYear):
+        #middle years 
+        df0 = fulldata.loc[fulldata['Year'] > StartYear]
+        df0 = df0.loc[df0['Year'] < EndYear]
+        toconcat.append(df0)
+        # first year
+        df1 = fulldata.loc[fulldata['Year'] == StartYear]
+        # first month
+        df2 = df1.loc[df1['Month'] == StartMonth]
+        df2 = df2.loc[df2['Day'] >= StartDay] 
+        df1 = df1.loc[df1['Month'] > StartMonth]
+        toconcat.append(df1)
+        toconcat.append(df2)
+        # last year
+        df3 = fulldata.loc[fulldata['Year'] == EndYear]
+        # first month
+        df4 = df3.loc[df3['Month'] == EndMonth]
+        df4 = df4.loc[df4['Day'] <= EndDay]    
+        df3 = df3.loc[df3['Month'] < EndMonth]
+        toconcat.append(df3)
+        toconcat.append(df4)
+        df = pd.concat(toconcat)
+        
+    return df
--- file2.py
+++ file2.py
@@ -1,1 +1,21 @@
-#A function that can acheive: For a user-selected period, produce a chart to show the number of accidents in each hour of the day (on average).from file_loader import fileloaderdata = fileloader()
+#A function that can acheive: For a user-selected period, produce a chart to show the number of accidents in each hour of the day (on average).
+
+from SelectedTimePeriod import select_Time_Period
+    
+SelDate = input("input Start date YYYY/MM/DD: ")
+EndDate = input("input End date YYYY/MM/DD: ")
+data = select_Time_Period()
+
+def sortHourOfDay(data, name):
+    data = data[[col for col in data]]
+    nRow, nCol = data.shape
+       
+    if (name == 'ACCIDENT_TIME'):
+        BarData = {'00': 0, '01': 0, '02': 0, '02': 0, '03': 0, '04': 0, '05': 0, '06': 0, '07': 0, '08': 0, '09': 0, '10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0, '21': 0, '22': 0, '23': 0}
+        for index, row in data.iterrows():
+            Hour = row['ACCIDENT_TIME'].split(".")[0]
+            BarData[Hour] +=1         
+    return BarData
+
+
+print(sortHourOfDay(data, 'ACCIDENT_TIME'))
