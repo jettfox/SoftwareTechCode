83b0a7678309b64d9ea77b8683f3fa9d046d28df
IAmCableGuy <62466388+IAmCableGuy@users.noreply.github.com>
2020-10-07 21:16:05 +1000

    file 3 update

--- file3.py
+++ file3.py
@@ -1,3 +1,20 @@
-#A function that can acheive: For a user-selected period, retrieve all accidents that contains a keyword (user entered) in the DCA_CODE, e.g. animal, ped.
-from file_loader import fileloader
-data = fileloader()
+# For a user-selected period, (DONE)
+# E.G. animal, ped. (user entered)
+# Retrieve all accidents that contains a keyword In the DCA_CODE,
+
+from selectedtimeperiod import select_Time_Period
+data = select_Time_Period()
+
+Keyword = input('Enter Keyword: ')
+
+
+#%%
+
+for lineNo in range(len(data['DCA_CODE'])):
+    currkeywords = []
+    currCode = data['DCA_CODE'][lineNo]
+    currkeywords = currCode.split()
+    
+    if (Keyword in currkeywords):
+        #savedata
+        print('placeholder code')
