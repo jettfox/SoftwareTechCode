c508876db4991df7e55d0fd8b5aa07a2a5cd23f0
CallumABeale <callum.beale.098@gmail.com>
2020-10-08 12:46:52 +1000

    Visual layout generally complete No event functionality included

--- DAVCrash.py
+++ DAVCrash.py
@@ -77,7 +77,7 @@ class frameMain (wx.Frame):
                               u"Light condition", u"Alcohol related", u"Severity"]
 		self.function1Selector = wx.Choice(
 			self.panelDef1, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, function1SelectorChoices, 0)
-		self.function1Selector.SetSelection(0)
+		self.function1Selector.SetSelection(1)
 		sizerFunc1.Add(self.function1Selector, 0, wx.ALL, 5)
 
 		self.function1Button = wx.Button(
@@ -93,120 +93,120 @@ class frameMain (wx.Frame):
 
 		self.panelDef2 = wx.Panel(self.panelMain, wx.ID_ANY,
 		                          wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL)
-		sizerFunc2 = wx.BoxSizer(wx.VERTICAL)
+		sizerPanelDef2 = wx.BoxSizer(wx.VERTICAL)
 
 		self.function2Title = wx.TextCtrl(self.panelDef2, wx.ID_ANY, u"function2",
 		                                  wx.DefaultPosition, wx.DefaultSize, wx.TE_CENTRE | wx.TE_READONLY)
 		self.function2Title.SetBackgroundColour(
 			wx.SystemSettings.GetColour(wx.SYS_COLOUR_INACTIVECAPTION))
 
-		sizerFunc2.Add(self.function2Title, 0, wx.ALL | wx.EXPAND, 5)
+		sizerPanelDef2.Add(self.function2Title, 0, wx.ALL | wx.EXPAND, 5)
 
 		self.function2Desc = wx.TextCtrl(self.panelDef2, wx.ID_ANY, u"This dataset will generate a graph based upon whether a crash occured in a high alcohol time period.\n\nDay of the week: Number of crashes by day of the week.\n\nLight condition: Number of crashes related to light conditions.\n\nAlcohol related: Number of crashes where alcohol was related.\n\nSeverity: Crashes categories by the severity of the incident.", wx.DefaultPosition, wx.DefaultSize, wx.TE_MULTILINE | wx.TE_READONLY)
 		self.function2Desc.SetBackgroundColour(
 			wx.SystemSettings.GetColour(wx.SYS_COLOUR_INACTIVECAPTION))
 
-		sizerFunc2.Add(self.function2Desc, 0, wx.ALL | wx.EXPAND, 5)
+		sizerPanelDef2.Add(self.function2Desc, 0, wx.ALL | wx.EXPAND, 5)
 
-		bSizer593 = wx.BoxSizer(wx.HORIZONTAL)
+		sizerFunc2 = wx.BoxSizer(wx.HORIZONTAL)
 
 		function2SelectorChoices = [u"Day of the week",
                               u"Light condition", u"Alcohol related", u"Severity"]
 		self.function2Selector = wx.Choice(
 			self.panelDef2, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, function2SelectorChoices, 0)
 		self.function2Selector.SetSelection(0)
-		bSizer593.Add(self.function2Selector, 0, wx.ALL, 5)
+		sizerFunc2.Add(self.function2Selector, 0, wx.ALL, 5)
 
 		self.function2Button = wx.Button(
 			self.panelDef2, wx.ID_ANY, u"Generate", wx.DefaultPosition, wx.DefaultSize, 0)
-		bSizer593.Add(self.function2Button, 0, wx.ALL | wx.SHAPED, 5)
+		sizerFunc2.Add(self.function2Button, 0, wx.ALL | wx.SHAPED, 5)
 
-		sizerFunc2.Add(bSizer593, 1, wx.EXPAND, 5)
+		sizerPanelDef2.Add(sizerFunc2, 1, wx.EXPAND, 5)
 
-		self.panelDef2.SetSizer(sizerFunc2)
+		self.panelDef2.SetSizer(sizerPanelDef2)
 		self.panelDef2.Layout()
-		sizerFunc2.Fit(self.panelDef2)
+		sizerPanelDef2.Fit(self.panelDef2)
 		gSizerMain.Add(self.panelDef2, 1, wx.EXPAND | wx.ALL, 5)
 
 		self.panelDef3 = wx.Panel(self.panelMain, wx.ID_ANY,
-		                          wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL)
-		sizerFunc3 = wx.BoxSizer(wx.VERTICAL)
+			wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL)
+		sizerPanelDef3 = wx.BoxSizer(wx.VERTICAL)
 
 		self.alcoholTimeTitle = wx.TextCtrl(self.panelDef3, wx.ID_ANY, u"Alcohol Time Analyser",
-		                                    wx.DefaultPosition, wx.DefaultSize, wx.TE_CENTRE | wx.TE_READONLY)
+			wx.DefaultPosition, wx.DefaultSize, wx.TE_CENTRE | wx.TE_READONLY)
 		self.alcoholTimeTitle.SetBackgroundColour(
 			wx.SystemSettings.GetColour(wx.SYS_COLOUR_INACTIVECAPTION))
 
-		sizerFunc3.Add(self.alcoholTimeTitle, 0, wx.ALL | wx.EXPAND, 5)
+		sizerPanelDef3.Add(self.alcoholTimeTitle, 0, wx.ALL | wx.EXPAND, 5)
 
 		self.alcoholTimeDescription = wx.TextCtrl(self.panelDef3, wx.ID_ANY, u"This dataset will generate a graph based upon whether a crash occured in a high alcohol time period.\n\nDay of the week: Number of crashes by day of the week.\n\nLight condition: Number of crashes related to light conditions.\n\nAlcohol related: Number of crashes where alcohol was related.\n\nSeverity: Crashes categories by the severity of the incident.", wx.DefaultPosition, wx.DefaultSize, wx.TE_MULTILINE | wx.TE_READONLY)
 		self.alcoholTimeDescription.SetBackgroundColour(
 			wx.SystemSettings.GetColour(wx.SYS_COLOUR_INACTIVECAPTION))
 
-		sizerFunc3.Add(self.alcoholTimeDescription, 0, wx.ALL | wx.EXPAND, 5)
+		sizerPanelDef3.Add(self.alcoholTimeDescription, 0, wx.ALL | wx.EXPAND, 5)
 
-		bSizer59 = wx.BoxSizer(wx.HORIZONTAL)
+		sizerFunc3 = wx.BoxSizer(wx.HORIZONTAL)
 
 		alcoholTimeSelectorChoices = [
 			u"Day of the week", u"Light condition", u"Alcohol related", u"Severity"]
 		self.alcoholTimeSelector = wx.Choice(
 			self.panelDef3, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, alcoholTimeSelectorChoices, 0)
 		self.alcoholTimeSelector.SetSelection(0)
-		bSizer59.Add(self.alcoholTimeSelector, 0, wx.ALL, 5)
+		sizerFunc3.Add(self.alcoholTimeSelector, 0, wx.ALL, 5)
 
 		self.alcoholTimeGraphButton = wx.Button(
 			self.panelDef3, wx.ID_ANY, u"Generate", wx.DefaultPosition, wx.DefaultSize, 0)
-		bSizer59.Add(self.alcoholTimeGraphButton, 0, wx.ALL | wx.SHAPED, 5)
+		sizerFunc3.Add(self.alcoholTimeGraphButton, 0, wx.ALL | wx.SHAPED, 5)
 
-		sizerFunc3.Add(bSizer59, 1, wx.EXPAND, 5)
+		sizerPanelDef3.Add(sizerFunc3, 1, wx.EXPAND, 5)
 
-		self.panelDef3.SetSizer(sizerFunc3)
+		self.panelDef3.SetSizer(sizerPanelDef3)
 		self.panelDef3.Layout()
-		sizerFunc3.Fit(self.panelDef3)
+		sizerPanelDef3.Fit(self.panelDef3)
 		gSizerMain.Add(self.panelDef3, 1, wx.EXPAND | wx.ALL, 5)
 
 		self.panelDef4 = wx.Panel(self.panelMain, wx.ID_ANY,
-		                          wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL)
-		sizerFunc4 = wx.BoxSizer(wx.VERTICAL)
+			wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL)
+		sizerPanelDef4 = wx.BoxSizer(wx.VERTICAL)
 
 		self.mapGeneratorTitle = wx.TextCtrl(
 			self.panelDef4, wx.ID_ANY, u"Map Generator", wx.DefaultPosition, wx.DefaultSize, wx.TE_CENTRE | wx.TE_READONLY)
 		self.mapGeneratorTitle.SetBackgroundColour(
 			wx.SystemSettings.GetColour(wx.SYS_COLOUR_INACTIVECAPTION))
 
-		sizerFunc4.Add(self.mapGeneratorTitle, 0, wx.ALL | wx.EXPAND, 5)
+		sizerPanelDef4.Add(self.mapGeneratorTitle, 0, wx.ALL | wx.EXPAND, 5)
 
 		self.mapGeneratorDescription = wx.TextCtrl(self.panelDef4, wx.ID_ANY, u"This dataset will generate a map that presents the data based on the following selectable categories:\n\nSeverity: The severity of injuries sustained by perons involved in each incident.\n\nAlcohol related: If alcohol was a critical factor as a cause of the incident.\n\nHit and run: If the incident was classified as a hit and run.", wx.DefaultPosition, wx.DefaultSize, wx.TE_MULTILINE | wx.TE_READONLY)
 		self.mapGeneratorDescription.SetBackgroundColour(
 			wx.SystemSettings.GetColour(wx.SYS_COLOUR_INACTIVECAPTION))
 
-		sizerFunc4.Add(self.mapGeneratorDescription, 0, wx.ALL | wx.EXPAND, 5)
+		sizerPanelDef4.Add(self.mapGeneratorDescription, 0, wx.ALL | wx.EXPAND, 5)
 
-		bSizer591 = wx.BoxSizer(wx.HORIZONTAL)
+		sizerFunc4 = wx.BoxSizer(wx.HORIZONTAL)
 
 		mapGeenratorCategorySelectorChoices = [
 			u"Severity", u"Alcohol related", u"Hit and run"]
 		self.mapGeenratorCategorySelector = wx.Choice(
 			self.panelDef4, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, mapGeenratorCategorySelectorChoices, 0)
 		self.mapGeenratorCategorySelector.SetSelection(0)
-		bSizer591.Add(self.mapGeenratorCategorySelector, 0, wx.ALL, 5)
+		sizerFunc4.Add(self.mapGeenratorCategorySelector, 0, wx.ALL, 5)
 
 		mapGeneratorYearSelectorChoices = [
 			u"Day of the week", u"Light condition", u"Alcohol related", u"Severity"]
 		self.mapGeneratorYearSelector = wx.Choice(
 			self.panelDef4, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, mapGeneratorYearSelectorChoices, 0)
 		self.mapGeneratorYearSelector.SetSelection(0)
-		bSizer591.Add(self.mapGeneratorYearSelector, 0, wx.ALL, 5)
+		sizerFunc4.Add(self.mapGeneratorYearSelector, 0, wx.ALL, 5)
 
 		self.mapGeneratorButton = wx.Button(
 			self.panelDef4, wx.ID_ANY, u"Generate", wx.DefaultPosition, wx.DefaultSize, 0)
-		bSizer591.Add(self.mapGeneratorButton, 0, wx.ALL | wx.SHAPED, 5)
+		sizerFunc4.Add(self.mapGeneratorButton, 0, wx.ALL | wx.SHAPED, 5)
 
-		sizerFunc4.Add(bSizer591, 1, wx.EXPAND, 5)
+		sizerPanelDef4.Add(sizerFunc4, 1, wx.EXPAND, 5)
 
-		self.panelDef4.SetSizer(sizerFunc4)
+		self.panelDef4.SetSizer(sizerPanelDef4)
 		self.panelDef4.Layout()
-		sizerFunc4.Fit(self.panelDef4)
+		sizerPanelDef4.Fit(self.panelDef4)
 		gSizerMain.Add(self.panelDef4, 1, wx.EXPAND | wx.ALL, 5)
 
 		self.panelMain.SetSizer(gSizerMain)
--- DAVCrashMain.py
+++ DAVCrashMain.py
@@ -84,24 +84,125 @@ class myGui(wx.Frame):
     self.panelDef2 = wx.Panel(self.panelMain, wx.ID_ANY,
       wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL)
     # Function 2 main box sizer
-    sizerFunc2 = wx.BoxSizer(wx.VERTICAL)
+    sizerPanelDef2 = wx.BoxSizer(wx.VERTICAL)
     # Function 2 title
     self.function2Title = wx.TextCtrl(self.panelDef2, wx.ID_ANY, "function2",
       wx.DefaultPosition, wx.DefaultSize, wx.TE_CENTRE | wx.TE_READONLY)
-    # Function 
+    # Function
     self.function2Title.SetBackgroundColour(
       wx.SystemSettings.GetColour(wx.SYS_COLOUR_INACTIVECAPTION))
+    # Add function 2 Title to sizerFunction2
+    sizerPanelDef2.Add(self.function2Title, 0, wx.ALL | wx.EXPAND, 5)
+    # Create function 2 description and add background colour
+    self.function2Desc = wx.TextCtrl(self.panelDef2, wx.ID_ANY, "This dataset will generate a graph based upon whether a crash occured in a high alcohol time period.\n\nDay of the week: Number of crashes by day of the week.\n\nLight condition: Number of crashes related to light conditions.\n\nAlcohol related: Number of crashes where alcohol was related.\n\nSeverity: Crashes categories by the severity of the incident.", wx.DefaultPosition, wx.DefaultSize, wx.TE_MULTILINE | wx.TE_READONLY)
+    self.function2Desc.SetBackgroundColour(wx.SystemSettings.GetColour(wx.SYS_COLOUR_INACTIVECAPTION))
+    # Add function 2 description into sizerFunc2
+    sizerPanelDef2.Add(self.function2Desc, 0, wx.ALL | wx.EXPAND, 5)
+
+    sizerFunc2 = wx.BoxSizer(wx.HORIZONTAL)
+
+    function2SelectorChoices = ["Day of the week",
+      "Light condition", "Alcohol related", "Severity"]
+    self.function2Selector = wx.Choice(
+      self.panelDef2, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, function2SelectorChoices, 0)
+    self.function2Selector.SetSelection(0)
+    sizerFunc2.Add(self.function2Selector, 0, wx.ALL, 5)
+
+    self.function2Button = wx.Button(
+      self.panelDef2, wx.ID_ANY, u"Generate", wx.DefaultPosition, wx.DefaultSize, 0)
+    sizerFunc2.Add(self.function2Button, 0, wx.ALL | wx.SHAPED, 5)
+
+    sizerPanelDef2.Add(sizerFunc2, 1, wx.EXPAND, 5)
+
+    self.panelDef2.SetSizer(sizerPanelDef2)
+    self.panelDef2.Layout()
+    sizerPanelDef2.Fit(self.panelDef2)
+    gSizerMain.Add(self.panelDef2, 1, wx.EXPAND | wx.ALL, 5)
+
+    self.panelDef3 = wx.Panel(self.panelMain, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL)
+    sizerPanelDef3 = wx.BoxSizer(wx.VERTICAL)
+
+    self.alcoholTimeTitle = wx.TextCtrl(self.panelDef3, wx.ID_ANY, "Alcohol Time Analyser",
+      wx.DefaultPosition, wx.DefaultSize, wx.TE_CENTRE | wx.TE_READONLY)
+    self.alcoholTimeTitle.SetBackgroundColour(
+      wx.SystemSettings.GetColour(wx.SYS_COLOUR_INACTIVECAPTION))
+
+    sizerPanelDef3.Add(self.alcoholTimeTitle, 0, wx.ALL | wx.EXPAND, 5)
+
+    self.alcoholTimeDescription = wx.TextCtrl(self.panelDef3, wx.ID_ANY, u"This dataset will generate a graph based upon whether a crash occured in a high alcohol time period.\n\nDay of the week: Number of crashes by day of the week.\n\nLight condition: Number of crashes related to light conditions.\n\nAlcohol related: Number of crashes where alcohol was related.\n\nSeverity: Crashes categories by the severity of the incident.", wx.DefaultPosition, wx.DefaultSize, wx.TE_MULTILINE | wx.TE_READONLY)
+    self.alcoholTimeDescription.SetBackgroundColour(wx.SystemSettings.GetColour(wx.SYS_COLOUR_INACTIVECAPTION))
+
+    sizerPanelDef3.Add(self.alcoholTimeDescription, 0, wx.ALL | wx.EXPAND, 5)
+
+    sizerFunc3 = wx.BoxSizer(wx.HORIZONTAL)
 
 
+    alcoholTimeSelectorChoices = [
+      u"Day of the week", u"Light condition", u"Alcohol related", u"Severity"]
+    self.alcoholTimeSelector = wx.Choice(self.panelDef3, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, alcoholTimeSelectorChoices, 0)
+    self.alcoholTimeSelector.SetSelection(0)
+
+    sizerFunc3.Add(self.alcoholTimeSelector, 0, wx.ALL, 5)
 
 
+    self.alcoholTimeGraphButton = wx.Button(
+      self.panelDef3, wx.ID_ANY, u"Generate", wx.DefaultPosition, wx.DefaultSize, 0)
+    sizerFunc3.Add(self.alcoholTimeGraphButton, 0, wx.ALL | wx.SHAPED, 5)
+
+    sizerPanelDef3.Add(sizerFunc3, 1, wx.EXPAND, 5)
 
 
+    self.panelDef3.SetSizer(sizerPanelDef3)
+    self.panelDef3.Layout()
+    sizerPanelDef3.Fit(self.panelDef3)
+    gSizerMain.Add(self.panelDef3, 1, wx.EXPAND | wx.ALL, 5)
+
+    # Function 4
+    self.panelDef4 = wx.Panel(self.panelMain, wx.ID_ANY,
+      wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL)
+    sizerPanelDef4 = wx.BoxSizer(wx.VERTICAL)
 
 
+    self.mapGeneratorTitle = wx.TextCtrl(
+      self.panelDef4, wx.ID_ANY, u"Map Generator", wx.DefaultPosition, wx.DefaultSize, wx.TE_CENTRE | wx.TE_READONLY)
+    self.mapGeneratorTitle.SetBackgroundColour(
+      wx.SystemSettings.GetColour(wx.SYS_COLOUR_INACTIVECAPTION))
+
+    sizerPanelDef4.Add(self.mapGeneratorTitle, 0, wx.ALL | wx.EXPAND, 5)
+
+    self.mapGeneratorDescription = wx.TextCtrl(self.panelDef4, wx.ID_ANY, "This dataset will generate a map that presents the data based on the following selectable categories:\n\nSeverity: The severity of injuries sustained by perons involved in each incident.\n\nAlcohol related: If alcohol was a critical factor as a cause of the incident.\n\nHit and run: If the incident was classified as a hit and run.", wx.DefaultPosition, wx.DefaultSize, wx.TE_MULTILINE | wx.TE_READONLY)
+    self.mapGeneratorDescription.SetBackgroundColour(
+      wx.SystemSettings.GetColour(wx.SYS_COLOUR_INACTIVECAPTION))
+
+    sizerPanelDef4.Add(self.mapGeneratorDescription, 0, wx.ALL | wx.EXPAND, 5)
+
+    sizerFunc4 = wx.BoxSizer(wx.HORIZONTAL)
 
 
+    mapGeenratorCategorySelectorChoices = [
+      "Severity", "Alcohol related", "Hit and run"]
+    self.mapGeenratorCategorySelector = wx.Choice(
+      self.panelDef4, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, mapGeenratorCategorySelectorChoices, 0)
+    self.mapGeenratorCategorySelector.SetSelection(0)
+    sizerFunc4.Add(self.mapGeenratorCategorySelector, 0, wx.ALL, 5)
 
+    mapGeneratorYearSelectorChoices = [
+      "Day of the week", "Light condition", "Alcohol related", "Severity"]
+    self.mapGeneratorYearSelector = wx.Choice(
+      self.panelDef4, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, mapGeneratorYearSelectorChoices, 0)
+    self.mapGeneratorYearSelector.SetSelection(0)
+    sizerFunc4.Add(self.mapGeneratorYearSelector, 0, wx.ALL, 5)
+
+    self.mapGeneratorButton = wx.Button(
+      self.panelDef4, wx.ID_ANY, u"Generate", wx.DefaultPosition, wx.DefaultSize, 0)
+    sizerFunc4.Add(self.mapGeneratorButton, 0, wx.ALL | wx.SHAPED, 5)
+
+    sizerPanelDef4.Add(sizerFunc4, 1, wx.EXPAND, 5)
+
+    self.panelDef4.SetSizer(sizerPanelDef4)
+    self.panelDef4.Layout()
+    sizerPanelDef4.Fit(self.panelDef4)
+    gSizerMain.Add(self.panelDef4, 1, wx.EXPAND | wx.ALL, 5)
 
 
     # Main panel set sizer & layout
@@ -117,6 +218,14 @@ class myGui(wx.Frame):
     # Show the App
     self.Show(True)
 
+
+    # # Connect Events
+    # self.Bind(wx.EVT_MENU, self.menuItemFileNewOnMenuSelection,
+    #   id=self.menuItemFileAbout.GetId())
+    # self.Bind(wx.EVT_MENU, self.menuItemFileOpenOnMenuSelection,
+    #   id=self.menuItemFileHelp.GetId())
+    # self.Bind(wx.EVT_MENU, self.menuItemFileExitOnMenuSelection,
+    #   id=self.menuItemFileExit.GetId())
 
 if __name__ == "__main__":
   app = wx.App()
